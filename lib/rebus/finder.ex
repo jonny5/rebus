528e23f1ea52473a0d648039837056851b9f1806
jonny5 <jonandrewkaplan@gmail.com>
2017-02-26 16:02:31 -0800

    Move word node to its own file

--- lib/rebus/finder.ex
+++ lib/rebus/finder.ex
@@ -1,25 +1,5 @@
 require IEx
 
-defmodule Rebus.WordNode do
-  defstruct [:remainder, :operator, :name, :children, :depth]
-  def print(nodes) when is_list(nodes) do
-    nodes |>
-    Enum.map(&print/1)
-  end
-  def print(node) do
-    cond do
-      node.children ->
-        responses = Enum.map(node.children, fn(child) -> print(child) end)
-        elements = Enum.join(responses, " #{node.operator} ")
-        "(#{elements})"
-      node.name ->
-        node.name
-      node.remainder ->
-        node.remainder
-    end
-  end
-end
-
 defmodule Rebus.Finder do
   import Ecto.Query
   alias Rebus.{Word, Repo, WordNode, StringSubsets}
---
+++ lib/rebus/word_node.ex
@@ -0,0 +1,19 @@
+defmodule Rebus.WordNode do
+  defstruct [:remainder, :operator, :name, :children, :depth]
+  def print(nodes) when is_list(nodes) do
+    nodes |>
+    Enum.map(&print/1)
+  end
+  def print(node) do
+    cond do
+      node.children ->
+        responses = Enum.map(node.children, fn(child) -> print(child) end)
+        elements = Enum.join(responses, " #{node.operator} ")
+        "(#{elements})"
+      node.name ->
+        node.name
+      node.remainder ->
+        node.remainder
+    end
+  end
+end
